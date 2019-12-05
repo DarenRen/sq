5228a323e55ed377b1798a2fdb4453fc855c51be
Daren Ren <dr3055@columbia.edu>
2019-12-05 01:20:01 +0000

    Write views and templates for /sightings

--- tracker/sightings/views.py
+++ tracker/sightings/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Sq
+from .forms import SqForm
+def all_sq(request):
+    sqs = Sq.objects.all()
+    context = {
+        'Squirrels': sqs,
+    }
+    return render(request, 'sightings/all.html', context)
 
-def index(request):
-    return HttpResponse('hi, aaaa')
-# Create your views here.
