8e734397b04c2ffbeda177d3112834102155e066
Wunan Hou <wh2455@columbia.edu>
2019-12-05 03:16:23 +0000

    update sightings/urls.py

--- tracker/sightings/urls.py
+++ tracker/sightings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.all_sq)
+    path('', views.all_sq),
     path('<str:Unique_Squirrel_ID>/', views.edit_sq, name = 'edit'),
     ]
