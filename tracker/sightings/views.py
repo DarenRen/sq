f7c078dd2e1dbecf5f30ca769abf50e594b570a4
Wunan Hou <wh2455@columbia.edu>
2019-12-06 04:43:28 +0000

    change map.html

--- tracker/sightings/views.py
+++ tracker/sightings/views.py
@@ -43,8 +43,8 @@ def stats(request):
     sqs = Sq.objects.all()
     attributes = ['Age','Primary_Fur_Color','Running','Chasing','Climbing','Eating','Foraging','Kuks','Quaas']
     values = {a:{} for a in attributes}
-    for key in  values.keys():
-        for val_pari in sqs.values(key).distinct():
+ #   for key in  values.keys():
+ #       for val_pari in sqs.values(key).distinct():
 
         
 
