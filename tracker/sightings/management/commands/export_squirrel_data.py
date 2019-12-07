4fd0eb6a3c8b22bcb55b8b0fe260353691485f46
Daren Ren <dr3055@columbia.edu>
2019-12-07 05:18:19 +0000

    Write export.py

--- tracker/sightings/management/commands/export_squirrel_data.py
+++ tracker/sightings/management/commands/export_squirrel_data.py
@@ -1,13 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 import csv
-import sys
 from sightings.models import Sq
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('file_name')
+
     def handle(self, *args, **options):
+        file_name = options['file_name']
         field_names = [f.name for f in Sq._meta.fields]
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
-        writer.writerow(field_names)
-        for instance in model.objects.all():
-            writer.writerow([unicode(getattr(instance, f)).encode('utf-8') for f in field_names])
+        with open(file_name, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(field_names)
+            for instance in Sq.objects.all():
+                writer.writerow(getattr(instance, f) for f in field_names)
 
