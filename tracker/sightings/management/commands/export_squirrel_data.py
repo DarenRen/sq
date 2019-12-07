f145e4c5c8fd779d7c161ad9050e33eecda392e0
Wunan Hou <wh2455@columbia.edu>
2019-12-07 05:19:07 +0000

    write main. and update export,stats.html

--- tracker/sightings/management/commands/export_squirrel_data.py
+++ tracker/sightings/management/commands/export_squirrel_data.py
@@ -2,12 +2,16 @@ from django.core.management.base import BaseCommand, CommandError
 import csv
 import sys
 from sightings.models import Sq
+from __future__ import unicode_literals
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+
     def handle(self, *args, **options):
         field_names = [f.name for f in Sq._meta.fields]
         writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
         writer.writerow(field_names)
-        for instance in model.objects.all():
-            writer.writerow([unicode(getattr(instance, f)).encode('utf-8') for f in field_names])
+        for instance in Sq.objects.all():
+            writer.writerow(getattr(instance, f) for f in field_names])
 
