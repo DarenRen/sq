d0fc6202d90496cabdbff2976400aef2b5647e37
darren <56941088+DarenRen@users.noreply.github.com>
2019-12-09 20:47:38 -0500

    Update urls.py

--- tracker/sightings/urls.py
+++ tracker/sightings/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('add/', views.add_sq, name = 'add'),
     path('stats/',views.stats, name = 'stats'),
     path('<Unique_Squirrel_ID>/', views.edit_sq, name = 'edit'),
-    ]
+]
